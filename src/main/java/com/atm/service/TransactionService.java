1e3d8cc9c3cfc8c4ecbbedefb189365aa9b69655
Rajesh-mm <rajeshmm5321@gmail.com>
2023-04-25 00:01:27 +0530

    Project

---
+++ src/main/java/com/atm/service/TransactionService.java
@@ -0,0 +1,40 @@
+package com.atm.service;
+
+import com.atm.dao.Transaction;
+import com.atm.dao.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The TransactionService class provides business logic for an account
+ * 
+ * 
+ * 
+ */
+@Service
+public class TransactionService {
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    /**
+     * Function to save a transaction to the repository
+     * @param transaction
+     */
+    public void save(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public Transaction getTransaction() {
+        Transaction t = transactionRepository.findTopByOrderByTransactionIdDesc();
+        return t;
+    }
+
+        // Method to retrieve all transactions
+    public List<Transaction> getAllTransactions() {
+        // List<Transaction> transactions = new ArrayList<>();
+        return transactionRepository.findAll();
+    }
+}
